588754bbeb60a112474619a4787cb42f79f85c0e
Nathan <72998804+KeikakuDorito@users.noreply.github.com>
2022-10-05 13:37:37 -0400

    exe

--- Assets/_Scripts/Midterm/midtermScoreManager.cs
+++ Assets/_Scripts/Midterm/midtermScoreManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class midtermScoreManager : MonoBehaviour
 {
     public static midtermScoreManager instance;
+
+    public GameObject scoreUI;
+    public GameObject livesUI;
 
     int score; //Increments in 1000
 
@@ -20,18 +25,23 @@ public class midtermScoreManager : MonoBehaviour
         }
 
         Debug.Log("You have: " + lives + "Lives left.");
+        
     }
 
     public void ChangeScore(int scoreValue)
     {
         score = score + scoreValue; //Adds Score value on total score
         Debug.Log("The Score is: " + score);
+        scoreUI.GetComponentInChildren<TextMeshProUGUI>().text = "The Score is: " + score;
+
     }
 
     public void TakeDamage()
     {
         score = score - 1000;
         lives--;
+        scoreUI.GetComponentInChildren<TextMeshProUGUI>().text = "The Score is: " + score;
+        livesUI.GetComponentInChildren<TextMeshProUGUI>().text = "You have: " + lives + "Lives left.";
         Debug.Log("The Score is: " + score);
         Debug.Log("You have: " + lives + "Lives left.");
     }
